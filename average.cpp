c1839eaa6f888bb3d09fa9843f12cae546d547c2
tonkhao <chananchida_tha@cmu.ac.th>
2023-03-30 12:28:36 +0700

    jjjjjjj

--- average.cpp
+++ average.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    float sum=0;
+    int howlong = argc - 1;
+    float average = 0;
+    for (int i = 1; i <= howlong; i++)
+    {
+        sum += atof(argv[i]);
+    }
+    average = sum / howlong;
+    if (argc <= 1)
+    {
+        cout << "Please input numbers to find average.";
+    }
+    else
+    {
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << howlong << " numbers = " << average;
+        cout << "\n---------------------------------";
+    }
+}
